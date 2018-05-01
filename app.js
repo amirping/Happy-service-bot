3e496156ec7fc899ce232b151421a1d214ab330d
Med Amir Saadallah <amirs-m-s@live.com>
2018-05-01 02:32:07 +0100

    start new

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
+
+// Server index route 
+app.get("/", function (req, res) {
+  res.send("Deployed! BOT server is on");
+});
+app.post("/", function (req, res) {
+  res.send(req);
+})
